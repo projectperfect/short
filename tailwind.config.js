616acca62b7f6d39569249814733b887c63bb117
projectperfect <jprfct@protonmail.com>
2022-09-18 06:20:09 -0500

    shorten section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
       colors : {
         cyan: 'hsl(180, 66%, 49%)',
         cyanLight: 'hsl(180, 66%, 69%)',
-        darkViolet: 'hasl(257, 27%, 26%)',
+        darkViolet: 'hsl(257, 27%, 26%)',
         red: 'hsl(0, 87%, 67%)',
         grayishViolet: 'hsl(257, 7%, 63%)',
         veryDarkBlue: 'hsl(255, 11%, 22%)',
